f281f5e98ae96a08b12080e098fde5ce3f768bb5
Nando <fernando.deleon@gmail.com>
2026-03-17 15:52:25 +1100

    Initial Commit

---
+++ MusicAi/MusicAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAiApp.swift
+//  MusicAi
+//
+//  Created by Fernando De Leon on 17/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
